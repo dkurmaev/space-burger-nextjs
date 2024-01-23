b0641a9ba92f7da2628af303bcdf0175b5e5fa4c
dkurmaev <danilkurmayev@gmail.com>
2024-01-23 17:20:49 +0100

    updated

--- src/app/api/profile/route.js
+++ src/app/api/profile/route.js
@@ -4,42 +4,30 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { User } from "@/models/User";
 import { UserInfo } from "@/models/UserInfo";
 
-export default async function handler(req, res) {
-  const { method } = req;
-
-  if (method === "PUT") {
-    return handlePUT(req, res);
-  } else if (method === "GET") {
-    return handleGET(req, res);
-  } else {
-    return res.status(405).end(); // Method Not Allowed
-  }
-}
-
-async function handlePUT(req, res) {
+export async function PUT(req) {
   mongoose.connect(process.env.MONGO_URL);
-  const data = await req.body;
+  const data = await req.json();
   const { name, image, ...otherUserInfo } = data;
   const session = await getServerSession(authOptions);
   const email = session.user.email;
 
-  // update user name
+  //update user name
   await User.updateOne({ email }, { name, image });
   await UserInfo.findOneAndUpdate({ email }, otherUserInfo, {
     upsert: true,
   });
 
-  return res.json(true);
+  return Response.json(true);
 }
 
-async function handleGET(req, res) {
+export async function GET() {
   mongoose.connect(process.env.MONGO_URL);
   const session = await getServerSession(authOptions);
   const email = session?.user?.email;
   if (!email) {
-    return res.json({});
+    return Response.json({});
   }
   const user = await User.findOne({ email }).lean();
   const userInfo = await UserInfo.findOne({ email }).lean();
-  return res.json({ ...user, ...userInfo });
+  return Response.json({ ...user, ...userInfo });
 }
